c779be5d4411244d652dfcd8348a752f64b3328a
laugh0608 <laugh0608@foxmail.com>
2025-03-27 14:24:41 +0800

    减少using语句

--- RazorPagesTestMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesTestMovie/Pages/Movies/Index.cshtml.cs
@@ -1,11 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using RazorPagesTestMovie.Data;
+// using RazorPagesTestMovie.Data;
 using RazorPagesTestMovie.Models;
 
 namespace RazorPagesTestMovie.Pages.Movies
